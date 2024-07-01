b08b813718e4899196a99f7229866874e9b294e3
A <cheoa@users.noreply.github.com>
2024-07-01 07:44:38 +0000

    11111

--- data/config.py
+++ data/config.py
@@ -2,15 +2,15 @@ MIN_PROXY_SCORE = 50
 
 CLAIM_REWARDS_ONLY = False  # claim tiers rewards only (https://app.getgrass.io/dashboard/referral-program)
 
-THREADS = 1  # for register account / claim rewards mode only
+THREADS = 1 # for register account / claim rewards mode only
 
-CHECK_POINTS = False  # show point for each account every nearly 10 minutes
+CHECK_POINTS = True  # show point for each account every nearly 10 minutes
 
 STOP_ACCOUNTS_WHEN_SITE_IS_DOWN = True  # stop account for 20 minutes, to reduce proxy traffic usage
 
 SHOW_LOGS_RARELY = True  # not always show info about actions to decrease pc influence
 
-APPROVE_EMAIL_ONLY = True  # approve email (without imap and access to email)
+APPROVE_EMAIL_ONLY = False  # approve email (without imap and access to email)
 
 # REGISTER PARAMETRS ONLY
 REGISTER_ACCOUNT_ONLY = False
@@ -26,7 +26,7 @@ CAPTCHAAI_API_KEY = ""
 CAPTCHA_PARAMS = {
     "captcha_type": "v2",
     "invisible_captcha": False,
-    "sitekey": "6LdyCj0pAAAAAFvvSTRHYOzddUPMPcH232u7a9e0",
+    "sitekey": "6LeeT-0pAAAAAFJ5JnCpNcbYCBcAerNHlkK4nm6y",
     "captcha_url": "https://app.getgrass.io/register"
 }
 
